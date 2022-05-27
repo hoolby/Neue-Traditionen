8b4fdb805e7667724c6dcd970e42a15846e61365
codeus-ex-machina <thegreatjordan@gmx.de>
2022-05-27 18:36:13 +0200

    registration page without design, first version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import Home from "@pages/Home";
+import React from "react";
+import Register from "@components/Register";
 
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const userMail = "testmail@testmail.com";
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Register userMail={userMail} />
     </div>
   );
 }
