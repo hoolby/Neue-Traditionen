787a90b7f5926c8fe33b31cc8c75ea285e22c58c
aylin <aylin.mrLarki@gmail.com>
2022-06-02 11:10:25 +0200

    home page update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import Home from "@pages/Home";
+import Home from "@pages/home/Home";
 import Navbar from "@components/nav/Navbar";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import Footer from "@components/footer/Footer";
 
 import "./App.css";
-
+library.add(fas, fab);
 function App() {
   return (
     <div className="App">
---
+++ frontend/src/components/items/BoxItems.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Items.css";
+
+const BoxItems = ({ items }) => {
+  return (
+    <>
+      <Link to={items.link} className="box-item">
+        <span className="box-item-icon">
+          <FontAwesomeIcon icon={items.icon} color="#156064" />
+        </span>
+        <p className="box-item-text">{items.text}</p>
+      </Link>
+    </>
+  );
+};
+export default BoxItems;
