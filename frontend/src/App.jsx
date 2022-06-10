0b883cfaf2943cbce90e64f097000c31c7e657b8
Moses Levin <moses.levin14@gmail.com>
2022-06-10 14:52:42 +0200

    merged pierreBranch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import Footer from "@components/footer/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Providers from "@components/providers/Providers";
+import BlogDetails from "@components/blogs/BlogDetails";
 
 import "./App.css";
 
@@ -27,6 +28,7 @@ function App() {
           <Route path="/register" element={<Register userMail={userMail} />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/providers" element={<Providers />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
         </Routes>
         <Footer />
       </Router>
