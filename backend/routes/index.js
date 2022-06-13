9b46ff0602f05565a331658bf2c189c753d75e6e
rupertberlin <ulrich.laitenberger@googlemail.com>
2022-06-13 14:05:43 +0200

    added routes/index.js

---
+++ backend/routes/index.js
@@ -0,0 +1,12 @@
+// TODO import routes like eg const moviesRouter = require('./movies');
+
+const setupRoutes = (app) => {
+  // User routes
+  // TODO add Routes like eg app.use('/api/movies', moviesRouter);
+ 
+};
+
+module.exports = {
+  setupRoutes,
+};
+
