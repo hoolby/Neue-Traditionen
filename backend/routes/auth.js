4c038cd3a80d254aaacbd4426bdb9a8f13d4636d
rupertberlin <ulrich.laitenberger@googlemail.com>
2022-06-13 15:34:51 +0200

    added roue/auth.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,24 @@
+const authRouter = require('express').Router();
+const User = require('../models/user');
+const { calculateToken } = require('../helpers/users');
+
+authRouter.post('/sessions', (req, res) => {
+  const { email, password } = req.body;
+  User.findByEmail(email).then((user) => {
+    if (!user) res.status(401).send('Invalid credentials');
+    else {
+      User.verifyPassword(password, user.hashedPassword).then(
+        (passwordIsCorrect) => {
+          if (passwordIsCorrect) {
+            const token = calculateToken(email);
+            res.cookie('user_token', token)
+            res.send()
+          }
+          else res.status(401).send('Invalid credentials');
+        }
+      );
+    }
+  });
+});
+
+module.exports = authRouter;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,11 @@
 const blogRouter = require('./blog');
 const providersRouter = require('./providers');
+const authRouter = require('./auth');
 
-blogRouter = require('./blog');
-providersRouter = require('./providers');
 
 const setupRoutes = (app) => {
   // User routes
-  // TODO add missing Routes like e.g. api/auth
+  app('api/auth', authRouter);
   app('api/blog', blogRouter);
   app('api/providers', providersRouter); 
 };
