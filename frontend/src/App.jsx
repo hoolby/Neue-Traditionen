75c46817d8103912939b9780e111057d36c61010
codeus-ex-machina <thegreatjordan@gmx.de>
2022-06-13 14:18:33 +0200

    translations, minor code-cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,28 @@
 /* eslint-disable import/no-unresolved */
+// Pages and components
 import BlogDetails from "@components/blogs/BlogDetails";
 import Footer from "@components/footer/Footer";
 import Navbar from "@components/nav/Navbar";
 import Providers from "@components/providers/Providers";
 import Register from "@components/Register";
+import AdminPage from "@pages/adminpage/Adminpage";
+import Home from "@pages/home/Home";
+
+// Modules
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import AdminPage from "@pages/adminpage/Adminpage";
-import Home from "@pages/home/Home";
+library.add(fas, fab);
+
+// CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-library.add(fas, fab);
-
-library.add(fas, fab);
 function App() {
-  const userMail = "testmail@testmail.com";
+  const userMail = "testmail@testmail.com"; // for Register Component - to be replaced by user email on entering the registration page from custom link
   return (
     <div className="App">
       <Router>
--- frontend/src/components/items/ItemsCopyForRegistrationPage.jsx
+++ frontend/src/components/items/ItemsCopyForRegistrationPage.jsx
@@ -1,30 +1,25 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import BoxItems from "./BoxItems";
-import Checkbox from "./Checkbox";
 import "./Items.css";
 
-const Items = () => {
-  const [infos, setInfos] = useState([
+function Items() {
+  const [infos] = useState([
     {
       link: "/",
       icon: "fa-solid fa-sliders",
-      text: "Choosing items for the burial",
+      text: "Elemente für die Bestattung auswählen",
     },
     {
       link: "/",
       icon: "fa-solid fa-scroll",
-      text: "Download a list of possible costs as a guide",
+      text: "Kostenorientierung herunterladen",
     },
     {
       link: "/",
       icon: "fa-solid fa-list-check",
-      text: "Check off the checklist with your to-dos & save everything online",
+      text: "Checkliste für deine Planung abhaken und online speichern",
     },
   ]);
-  const [checkedOne] = React.useState(true);
-  const [checkedTwo] = React.useState(true);
-  const [checkedThree] = React.useState(true);
 
   return (
     <>
@@ -35,5 +30,5 @@ const Items = () => {
       </div>
     </>
   );
-};
+}
 export default Items;
