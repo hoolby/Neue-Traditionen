3497573076d17518a7e700488fa8702b2d9a5bda
rupertberlin <ulrich.laitenberger@googlemail.com>
2022-06-14 12:55:44 +0200

    added some blog routes and model methods

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,3 +1,44 @@
+const connection = require('../db-config');
+const Joi = require('joi');
+
+const db = connection.promise();
+
 // TODO add methods to handle querie e.G. like e.G const findOne = (id) => { ... }
 
+const validate = (data, forCreation = true) => {
+    const presence = forCreation ? 'required' : 'optional';
+    return Joi.object({
+      title: Joi.string().max(255).presence(presence),
+      author: Joi.string().max(255).presence(presence),
+      picture: Joi.string().max(255),
+      text: Joi.string().presence(presence),
+    }).validate(data, { abortEarly: false }).error;
+  };
+
+
+const create = ({ title, author, picture, text, date }) => {
+    return db
+      .query(
+        'INSERT INTO blog (title, author, picture, text, date) VALUES (?, ?, ?, ?, ?)',
+        [title, author, picture, text, date]
+      )
+      .then(([result]) => {
+        const id = result.insertId;
+        return { id, title, author, picture, text, date };
+      });
+  };
+
+
+const findLast = (numberOfPosts) => {
+    let sql = 'SELECT * FROM blog ORDER BY id desc limit ?;';
+    const sqlValues = [numberOfPosts];
+  
+    return db.query(sql, sqlValues).then(([posts]) => posts);
+  };
+
 // TODO add exports of methods to use in routes like e.G. module.exports = { findOne };
+module.exports = {
+    validate,
+    create,
+    findLast,
+};
--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -3,4 +3,31 @@ const Blog = require('../models/blog');
 
 // TODO add Routes for blog
 
+// Get latest N posts
+blogRouter.get('/', async (req, res) => {
+    Blog.findLast(numberOfPosts=8)
+      .then((posts) => {
+        res.json(posts);
+      })
+      .catch((err) => {
+        res.status(500).send('Error retrieving blog posts from database');
+      });
+  });
+
+  blogRouter.post('/', (req, res) => {
+    const error = Blog.validate(req.body);
+    if (error) {
+      res.status(422).json({ validationErrors: error.details });
+    } else {
+      Blog.create(req.body)
+        .then((createdPost) => {
+          res.status(201).json(createdPost);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error saving the post');
+        });
+    }
+  });
+
 module.exports = blogRouter;
