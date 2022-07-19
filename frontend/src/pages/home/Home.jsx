51cd55889dfb2cf757e5ca34496ac77203368d93
codeus-ex-machina <thegreatjordan@gmx.de>
2022-07-19 15:51:30 +0200

    Homepage Section 1 done

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import useFetch from "@components/blogs/useFetch";
 import Create from "@components/blogs/create";
 import Items from "../../components/items/Items";
 import AdminPage from "../adminpage/Adminpage";
+import HomepageSection1 from "@components/HomepageSection1";
 import "./Home.css";
 import "@components/blogs/blog.css";
 
@@ -13,23 +14,26 @@ function Home() {
   const [showAdmin, setShowAdmin] = useState(true);
   const { data, isPending, error } = useFetch("http://localhost:5000/blogs");
   return (
-    <div>
+    <>
       {!showAdmin ? (
         <AdminPage />
       ) : (
-        <section className="home-container">
-          <p className="item-title">Thats How it works</p>
-          <Items />
-          <button className="item-button">START NOW</button>
-          <div className="home">
-            {error && <div>{error}</div>}
-            {isPending && <div>Loading...</div>}
-            {data && <BlogList blogs={data} />}
-            <Create />
-          </div>
-        </section>
+        <>
+          <HomepageSection1 />
+          <section className="home-container">
+            <h5 className="mt-5 item-title">So funktioniert es:</h5>
+            <Items />
+            <button className="item-button">JETZT STARTEN</button>
+            <div className="home">
+              {error && <div>{error}</div>}
+              {isPending && <div>Loading...</div>}
+              {data && <BlogList blogs={data} />}
+              <Create />
+            </div>
+          </section>
+        </>
       )}
-    </div>
+    </>
   );
 }
 export default Home;
