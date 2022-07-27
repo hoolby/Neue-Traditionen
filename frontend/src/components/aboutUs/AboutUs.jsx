a50ee8d1ed49a8620a0654e679f4f080ae720df1
Moses Levin <moses.levin14@gmail.com>
2022-07-27 18:44:34 +0200

    blog and home fixes

--- frontend/src/components/aboutUs/AboutUs.jsx
+++ frontend/src/components/aboutUs/AboutUs.jsx
@@ -1,10 +1,20 @@
+import "./AboutUs.css";
+
 function AboutUs() {
   return (
     <div className="container">
-      <h1>The Team</h1>
+      <h1>NEUE TRADITIONEN ist</h1>
+      <br />
+      <br />
       <ul>
-        <li>team member 1</li>
-        <li>team member 2</li>
+        <li>📆Dein zeitgemäßer Bestattungsplaner</li>
+        <li>🤗Deine Community für Trauer</li>
+        <li>💗Dein Austausch über Rituale</li>
+        <br />
+        <li>
+          Wir helfen Dir durch die Zeit nach dem Verlust eines geliebten
+          Menschen.
+        </li>
       </ul>
     </div>
   );
