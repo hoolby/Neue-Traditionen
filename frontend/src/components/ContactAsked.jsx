d3dee406a30c3ec75b12a0899511e3a277fd506d
PIERRE AHSATAL <asathal.pierre@gmail.com>
2022-07-27 02:56:16 +0200

    see invitation asked OK

--- frontend/src/components/ContactAsked.jsx
+++ frontend/src/components/ContactAsked.jsx
@@ -1,6 +1,50 @@
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from "react";
+/* eslint-disable */
+/* eslint-disable react/prop-types */
+
+import { collection, getDocs, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+
+function ContactAsked() {
+  const [data, setData] = useState([]);
+  const fetchTalker = async () => {
+    try {
+      const q = query(collection(db, "talker"));
+      const docs = await getDocs(q);
+      const temp = [];
+      docs.forEach((doc) => {
+        temp.push({ ...doc.data() });
+      });
+      setData(temp);
+    } catch (err) {
+      console.error(err); 
+      console.log("An error occured while fetching talker data");
+    } console.log(data);
+  };
+
+  useEffect(() => {
+    fetchTalker();
+  }, []);
+
+  return (
+    <div>
+      {data.map((info) => (
+    <ul className="invitCard">
+        <li>{info.name}</li>
+        <li>{info.email}</li>
+        <li>{info.message}</li>
+      </ul>
+      ))}
+    </div>
+  );
+}
+
+export default ContactAsked;
+
+
+
+
+/* import React, { useState } from "react";
 import axios from "axios";
 import InvitCard from "@components/invitCard";
 
@@ -32,4 +76,4 @@ function ContactAsked() {
   );
 }
 
-export default ContactAsked;
+export default ContactAsked;  */
