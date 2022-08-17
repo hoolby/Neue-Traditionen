4194cedf155feac5e96b0f0711ecf03b8db2f1ce
PIERRE AHSATAL <asathal.pierre@gmail.com>
2022-08-17 12:30:05 +0200

    firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -19,12 +19,12 @@ import {
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC1tdKLi_OdO8bUxuesjHOTHHusCmUGgSo",
+  authDomain: "neue-traditionen-c1056.firebaseapp.com",
+  projectId: "neue-traditionen-c1056",
+  storageBucket: "neue-traditionen-c1056.appspot.com",
+  messagingSenderId: "607377803792",
+  appId: "1:607377803792:web:258b51512d197ae23c1895",
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
 };
 const app = initializeApp(firebaseConfig);
